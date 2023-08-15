9fc70f6f0dfe2aafd578415ec3322373f390db27
DzmitryR <dzmitryreshko@gmail.com>
2023-08-16 01:10:47 +0300

    added images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import s from './header.module.css';
+import logo from '../../image/001MinskObl.png';
 
 const Header = () => {
     return (
         <div className={s.headerBackground}>
             <div className={s.headerContainer}>
-                <h1 className={s.headerTitle}>УКС Миноблисполкома</h1>
+                <div className={s.headerTitleWrapper}>
+                    <img className={s.logo} src={logo} alt="logo"/>
+                    <h1 className={s.headerTitle}><span className={s.headerTitleSpan}>КОММУНАЛЬНОЕ УНИТАРНОЕ ПРЕДПРИЯТИЕ</span><br/>
+                        "УПРАВЛЕНИЕ КАПИТАЛЬНОГО СТРОИТЕЛЬСТВА МИНОБЛИСПОЛКОМА"</h1>
+                </div>
             </div>
         </div>
     );
