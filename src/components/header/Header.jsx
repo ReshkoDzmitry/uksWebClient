8483a63fcce26d765927688f64c30c6668b0b036
DzmitryR <dzmitryreshko@gmail.com>
2023-09-04 01:46:57 +0300

    added style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './header.module.css';
 import logo from '../../image/001MinskObl.png';
 import Navigation from "../navigation/Navigation";
+import imgFirstScreen from "../../image/1.jpg";
 
 const Header = () => {
     return (
@@ -12,16 +13,34 @@ const Header = () => {
                     <h1 className={s.headerTitle}><span
                         className={s.headerTitleSpan}>КОММУНАЛЬНОЕ УНИТАРНОЕ ПРЕДПРИЯТИЕ</span><br/>
                         "УПРАВЛЕНИЕ КАПИТАЛЬНОГО СТРОИТЕЛЬСТВА МИНОБЛИСПОЛКОМА"</h1>
-
                 </div>
 
-                <Navigation/>
-
+                <div className={s.navigationWrapper}>
+                    <Navigation/>
+                </div>
             </div>
 
-            {/*<div>*/}
-            {/*    <img className={s.house} src={house} alt="house"/>*/}
-            {/*</div>*/}
+            <div className={s.containerFirstScreen}>
+                {/*<img className={s.house} src={imgFirstScreen} alt="imgFirstScreen"/>*/}
+                <div className={s.wrapperTitleFirstScreen}>
+                    <h2 className={s.titleFirstScreen}>УКС МИНОБЛИСПОЛКОМА</h2>
+                    <h3 className={s.descriptionFirstScreen}>
+                        Полный спектр услуг <br/> по строительству и техническому надзору строительного процесса
+                    </h3>
+                </div>
+
+                <div className={s.wrapperContactFirstScreen}>
+                    <div className={s.wrapperContactPhone}>
+                        8 017 626-83-66
+                    </div>
+                    <div className={s.wrapperContactMail}>
+                        oblstroy@uksmoik.by
+                    </div>
+
+                </div>
+            </div>
+
+
         </div>
 
     );
