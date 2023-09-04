eed56201bf13fd8e66bb0359401e9dd46e9c0ab7
Nabls <sergikon92@gmail.com>
2023-09-04 22:07:29 +0300

    add info for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,25 @@ import s from './footer.module.css'
 
 const Footer = () => {
     return (
-        <div className={s.wrapper}>
-            
+        <div className={s.footer}>
+            <div className={s.wrapper}>
+                <div className={s.infoFooter}>
+                    <h4 className={s.infoTitle}>УКС МИНОБЛИСПОЛКОМА</h4>
+                    <p className={s.infoDescription}>Управление капитального строительства МИНОБЛИСПОЛКОМА предлагает полный спектр услуг по
+                        строительству и техническому надзору строительного процесса.</p>
+                </div>
+                <div className={s.infoFooter}>
+                    <h4 className={s.infoTitle}>Адрес предприятия</h4>
+                    <p className={s.infoDescription}>Интернациональная улица, 8 г. Минск</p>
+                    <p className={s.infoDescription}>Понедельник — пятница: с 8.00 до 17.00
+                        обед с 13.00 до 13.30</p>
+                </div>
+                <div className={s.infoFooter}>
+                    <h4 className={s.infoTitle}>Связаться с нами</h4>
+                    <p className={s.infoDescription}>8 017 626-83-66 приемная</p>
+                    <p className={s.infoDescription}>oblstroy@uksmoik.by</p>
+                </div>
+            </div>
         </div>
     );
 };
