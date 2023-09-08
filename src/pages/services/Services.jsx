e03a61183bfb131f3b398320b7e10ae27ab945bd
Nabls <sergikon92@gmail.com>
2023-09-08 19:52:11 +0300

    added file data, added style for component footer, added styles for component services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import s from './services.module.scss'
+import {services} from "../../data/data";
 
 const Services = () => {
     return (
-        <div>
-            Services
+        <div className={s.servicesWrapper}>
+            {
+                services.map(m=>{
+                    return (
+                        <div className={s.servicesInner}>
+                            <img className={s.servicesLogo} src="" alt=""/>
+                            <h4 className={s.servicesTitle}>{m.title}</h4>
+                            <p className={s.servicesNumber}>{m.phoneNumber}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
