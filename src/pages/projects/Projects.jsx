362eda104fc6b55620acab4d4379ec65adf388ec
Nabls <sergikon92@gmail.com>
2023-09-11 20:38:36 +0300

    added structure for component projects, data file

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import s from './projects.module.scss'
+import {projects} from "../../data/data";
 
 const Projects = () => {
     return (
-        <div>
-            Проекты
+        <div className={s.projectsWrapper}>
+            {
+                projects.map(m=> {
+                    return (
+                        <div className={s.projectsInner}>
+                            <h4 className={s.projectsTitle}>{m.title}</h4>
+                            <img className={s.projectsImg} src="" alt=""/>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
