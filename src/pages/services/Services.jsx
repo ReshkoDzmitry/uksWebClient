ef0f61bf78e57db251354a456f98292af03acd13
DzmitryR <dzmitryreshko@gmail.com>
2023-09-12 00:29:30 +0300

    refactor page services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -4,19 +4,50 @@ import {services} from "../../data/data";
 
 const Services = () => {
     return (
-        <div className={s.servicesWrapper}>
-            {
-                services.map(m=>{
-                    return (
-                        <div className={s.servicesInner}>
-                            <img className={s.servicesLogo} src="" alt=""/>
-                            <h4 className={s.servicesTitle}>{m.title}</h4>
-                            <p className={s.servicesNumber}>{m.phoneNumber}</p>
-                        </div>
-                    )
-                })
-            }
+        <div>
+
+            <div className={s.servicesTitleWrapper}>
+                <h2 className={s.servicesPageTitle}>
+                    Технический надзор и инженерные услуги
+                </h2>
+            </div>
+
+            <h3>
+                Инженерные услуги
+            </h3>
+            <div className={s.servicesWrapper}>
+                                {
+                    services.map(m => {
+                        return (
+                            <div className={s.servicesInner}>
+                                <img className={s.servicesLogo} src="" alt=""/>
+                                <h4 className={s.servicesTitle}>{m.title}</h4>
+                                <p className={s.servicesNumber}>{m.phoneNumber}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <h3>
+                Технический надзор
+            </h3>
+            <div className={s.servicesWrapper}>
+                {
+                    services.map(m => {
+                        return (
+                            <div className={s.servicesInner}>
+                                <img className={s.servicesLogo} src="" alt=""/>
+                                <h4 className={s.servicesTitle}>{m.title}</h4>
+                                <p className={s.servicesNumber}>{m.phoneNumber}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
+
     );
 };
 
