9ec20377c96bd811b951d3826c98b7e3a93182bf
Nabls <sergikon92@gmail.com>
2023-09-18 22:07:31 +0300

    added styles for component services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import s from './services.module.scss'
-import {services} from "../../data/data";
+import {engineeringServices, services, technicalSupervision} from "../../data/data";
 
 const Services = () => {
     return (
-        <>
+        /*<>
 
             <div className={s.servicesTitleWrapper}>
                 <h2 className={s.servicesPageTitle}>
@@ -44,6 +44,56 @@ const Services = () => {
                         )
                     })
                 }
+            </div>
+
+        </>*/
+        <>
+            <div className={s.servicesTitleWrapper}>
+                <h2 className={s.servicesPageTitle}>
+                    Технический надзор и инженерные услуги
+                </h2>
+            </div>
+
+            <div className={s.engineeringServicesWrapper}>
+                <div>
+                    <h3>Инженерные Услуги</h3>
+                    <p>ПО ОБЪЕКТАМ 1-4 КЛАССА СЛОЖНОСТИ:</p>
+                </div>
+                {
+                    engineeringServices.map(m=> {
+                        return (
+                            <div className={s.engineeringServicesInner}>
+                                <h4>{m.title}</h4>
+                                <p>{m.description}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div className={s.technical}>
+                <div className={s.technicalSupervision}>
+                    <div className={s.technicalSupervisionTitleWrapper}>
+                        <div className={s.engineeringServicesTitleInner}>
+                            <h4 className={s.engineeringServicesTitle}>Технический Надзор</h4>
+                            <p>ПРЕДПРИЯТИЕ «УКС МИНОБЛИСПОЛКОМА» ОСУЩЕСТВЛЯЕТ ТЕХНИЧЕСКИЙ КОНТРОЛЬ ВСЕХ СТАДИЙ СТРОИТЕЛЬНОГО ПРОЦЕССА:</p>
+                        </div>
+                        <img className={s.technicalSupervisionImg} src="" alt=""/>
+                    </div>
+
+                 <div className={s.technicalServicesInfo}>
+                     {
+                         technicalSupervision.map(m=>{
+                             return(
+                                 <div className={s.technicalServicesInfoInner}>
+                                     <h4>{m.title}</h4>
+                                     <p>{m.description}</p>
+                                 </div>
+                             )
+                         })
+                     }
+                 </div>
+                </div>
             </div>
 
         </>
